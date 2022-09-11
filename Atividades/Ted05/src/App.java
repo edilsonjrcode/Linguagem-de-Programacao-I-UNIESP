2093be790b0c2fbf1a40c6109ddf2bf5882b3bfc
Edilson Quirino Do Nascimento Júnior <95941206+edilsonjrcode@users.noreply.github.com>
2022-09-11 00:25:24 -0300

    primeiro commit

---
+++ Atividades/Ted05/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("LINGUAGEM DE PROGRAMAÇÃO I / PROF: MESSIAS BATISTA\n");
+        System.out.println("Atividade 05 / Aluno: Edilson Júnior\n");
+
+        int i = 1;
+
+        while (i <= 20) {
+            System.out.print(i);
+            if (i % 5 == 0) {
+                System.out.println();
+            } else {
+                System.out.print('\t');
+            }
+            i++;
+        }
+    }
+}
